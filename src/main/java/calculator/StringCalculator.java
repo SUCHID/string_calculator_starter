899f39a75d8160e96f52c34b9922242c6cf48dbf
D3_49550_SUCHID <suchidparbat@gmail.com>
2021-10-17 22:11:50 +0530

    delimitter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,6 +8,7 @@ import java.util.List;
 class StringCalculator {
 
   
+private String input;
 public int add(String input) {
     if (input.isEmpty()) {
     	return 0;
@@ -17,13 +18,49 @@ public int add(String input) {
     	return Integer.parseInt(input);
     }
 }
-public int calculate(String text) {
+public int calculate(String input) {
     List<String> negatives = new ArrayList<String>();
 
-    if (text == null || text.isEmpty()) {
+    if (input == null || input.isEmpty()) {
         return 0;
     }
 	return 0;
 }
 
+char Delimiter = getDelimiter(input.split("\n")[0]);
+
+String[] chuncks = (Delimiter == ',') ? 
+		input.split(",|\n") : input.split("\n|" + Delimiter);
+int total = 0;
+int skipFirstLines = (Delimiter == ',') ? 0 : 2;
+{
+
+
+StringCalculator negatives;
+for (String item : chuncks) {
+    if (skipFirstLines > 0) {
+        --skipFirstLines;
+    } else {
+        int num = Integer.parseInt(item);
+        if (num < 0) {
+            negatives.add(item);
+        }
+        total += num;
+    }
+}
+if (!negatives.isEmpty()) {
+    throw new IllegalArgumentException(
+        "negatives not allowed " + String.join(",", negatives));
+}
+private char getDelimiter(String string) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+return total;
+}
+
+private boolean isEmpty() {
+	
+	return false;
+}
 }
